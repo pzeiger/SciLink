c2084d657d15cdc0c1ab4805612492be4eac1fab
Maxim Ziatdinov <maxim.ziatdinov@pnnl.gov>
2026-02-11 08:47:57 -0800

    Get rid of now redundant workflows

--- scilink/cli/__init__.py
+++ scilink/cli/__init__.py
@@ -8,7 +8,5 @@ from . import plan
 from . import simulate
 from . import analyze
 
-from .workflows import main as experimental_novelty_main
-from .agents import add_agent_args
 
-__all__ = ['main', 'plan', 'simulate', 'analyze', 'experimental_novelty_main', 'add_agent_args']
+__all__ = ['main', 'plan', 'simulate', 'analyze']
